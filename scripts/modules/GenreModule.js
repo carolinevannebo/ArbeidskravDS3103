4453dc4e45c70bbf88240af6673c25a72ea8bb18
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2022-10-11 18:53:48 +0200

    test

--- scripts/modules/GenreModule.js
+++ scripts/modules/GenreModule.js
@@ -8,7 +8,7 @@ const GenreModule = ( () => {
     const getAllGenresFromArray = () => spotifyGenres;
 
     const isGenreValid = (genre) => {
-        return getAllGenresFromArray().includes(genre);
+        return getAllGenresFromArray().toLowerCase().includes(genre).toLowerCase();
     }
 
     return {
